ec311e3cc82d609cfd5b197cd1c93483680b2856
pramodya1994 <pramodya.mendis@obviohealth.com>
2023-07-21 16:59:37 +0800

    Add dynamic routes

--- 2_FirstNextJsApp/nextjs-blog/pages/index.js
+++ 2_FirstNextJsApp/nextjs-blog/pages/index.js
@@ -4,6 +4,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from "next/link";
 import Alert from "../components/alert/alert";
 import {getSortedPostsData} from "../lib/posts";
+import Date from '../components/date';
 
 export default function Home({allPostsData}) {
     return (
@@ -24,13 +25,13 @@ export default function Home({allPostsData}) {
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
-                    {allPostsData.map(({ id, date, title }) => (
+                    {allPostsData.map(({id, date, title}) => (
                         <li className={utilStyles.listItem} key={id}>
-                            {title}
-                            <br />
-                            {id}
-                            <br />
-                            {date}
+                            <Link href={`/posts/${id}`}>{title}</Link>
+                            <br/>
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date}/>
+                            </small>
                         </li>
                     ))}
                 </ul>
